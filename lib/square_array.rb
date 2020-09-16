dfa94d12370458c59b16642fe98b4f497b4a6923
Steven wu <stevenwu91@icloud.com>
2020-09-16 16:15:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,18 +1,13 @@
 numbers = [1,2,3]
 
 
-# def square_array(array)
-#   counter = 0
-#   while array[counter] do
-#     array[counter] = array[counter] ** 2
-#     counter += 1
-#   end
+def square_array(array)
+  counter = 0
+  while array[counter] do
+    array[counter] = array[counter] ** 2
+    counter += 1
+  end
   
 
-# end
-
-def square_array(array)
-  while array.length.times { |index|
-  array[index] = array[index] ** 2
-  }
 end
+
